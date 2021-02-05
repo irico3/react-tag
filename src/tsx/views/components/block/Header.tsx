be856f73bfea8ab52ba6b357b978d8da3cce815a
takanokana <iricocco3@gmail.com>
2021-02-05 13:31:36 +0900

    no message

--- src/tsx/views/components/block/Header.tsx
+++ src/tsx/views/components/block/Header.tsx
@@ -30,6 +30,7 @@ export default function Header() {
         <HeaderColorBtn clrType="red" />
         <HeaderColorBtn clrType="yellow" />
       </div>
+      <p>{userData.message}</p>
       <button
         onClick={() => setUserData({
           ...userData,
